ae498ef138eb50c613c066b44f39a0850bb4931d
Hunter M <dhmorse@calpoly.edu>
2021-06-22 23:02:39 -0700

    no updates- day with friends

--- GUI/view.py
+++ GUI/view.py
@@ -3,6 +3,7 @@ class MenuPage():
 	def __init__(self, title, options, prev_page):
 		self.title = title
 		self.options = options
-		5
+		self.prevpage = prevpage
 
-		self.index = 0
+		self.index = 0		# index of current selection
+		self.top_index = 0	# index of first entry on page
